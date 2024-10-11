98a0428799f344777c9a4a672148657fca3b3859
ntwalii <ntwaliaubin@gmail.com>
2024-10-11 17:21:32 +0200

    probalbility executable

---
+++ math/probability/exponential.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+
+"""
+Create an Exponential class to represent
+exponential distribution.
+"""
+
+
+class Exponential:
+    """
+    Method for exponential distribution
+    if lambtha value is below 0 there is an error
+    Change lambtha into a floating number
+    """
+    def __init__(self, data=None, lambtha=1.):
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            else:
+                self.lambtha = float(lambtha)
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            self.lambtha = 1 / (sum(data) / len(data))
+
+    """
+    Calculate the PDF of a given time Period.
+    """
+    def pdf(self, x):
+        """
+        x is the time period
+        Returns the PDF value for x
+        If x is out of range, return 0
+        """
+        if x < 0:
+            return 0
+        return self.lambtha * 2.7182818285 ** (-self.lambtha * x)
+
+    def cdf(self, x):
+        """
+        Calculates the value of the CDF for a given time period
+        x = time period
+        """
+        if x < 0:
+            return 0
+        return 1 - 2.7182818285 ** (-self.lambtha * x)
---
+++ math/probability/poisson.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+"""
+Defining a class called Poisson.
+"""
+
+
+class Poisson:
+    """
+    __init__: used to initialize a method for the class instance.
+    lambtha <= 0: It must be a positive value.
+
+    """
+    def __init__(self, data=None, lambtha=1.):
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            else:
+                self.lambtha = float(lambtha)
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            self.lambtha = float(sum(data) / len(data))
+
+    """ A method that calculates the value of
+    the PMF for a given number of successes.
+    """
+    def pmf(self, k):
+        """
+        Calculates the value of the PMF for a given number of successes
+        """
+        if not isinstance(k, int):
+            k = int(k)
+        if k < 0:
+            return 0
+        factorial = 1
+        for i in range(1, k + 1):
+            factorial = factorial * i
+        result = self.lambtha ** k * 2.7182818285 ** (-self.lambtha)
+        return result / factorial
+
+    """
+    A method that calculates the value of CDF
+    for a given number of successes.
+    """
+
+    def cdf(self, k):
+        """
+        Convert k into an integer
+        Return 0 if k is below zero
+        """
+        k = int(k)
+        if k < 0:
+            return 0
+        cdf = 0
+        for i in range(k + 1):
+            cdf += self.pmf(i)
+        return cdf
