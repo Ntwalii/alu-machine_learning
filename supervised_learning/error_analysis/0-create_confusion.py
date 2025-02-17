85f637f013e82b85cae74c40cc038a8586ad46ca
ntwalii <ntwaliaubin@gmail.com>
2025-02-17 14:45:54 +0200

    Error analysis

---
+++ supervised_learning/error_analysis/0-create_confusion.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Script to create a confusion matrix"""
+
+import numpy as np
+
+
+def create_confusion_matrix(labels, logits):
+    """
+    Function to create a confusion matrix
+    Args:
+        labels: one-hot numpy.ndarray of shape (m, classes)
+                   m: is the number of data points
+                   classes: is the number of classes
+        logits: one-hot numpy.ndarray of shape (m, classes)
+                containing the predicted labels
+    """
+    return np.matmul(labels.T, logits)
