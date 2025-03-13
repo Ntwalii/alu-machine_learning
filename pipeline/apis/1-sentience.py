c4724deb97a374694eb27d73df163e98768798d0
ntwalii <ntwaliaubin@gmail.com>
2025-03-13 22:21:23 +0200

    pipeline

---
+++ pipeline/apis/1-sentience.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+""" Get requests v2"""
+import requests
+
+
+def sentientPlanets():
+    """
+    Function to get request from names of planets
+    Returns: List of names of sentient species
+    """
+    planets = []
+    url = 'https://swapi-api.hbtn.io/api/species/'
+    while url is not None:
+        response = requests.get(url,
+                                headers={'Accept': 'application/json'},
+                                params={"term": 'specie'})
+        for specie in response.json()['results']:
+            if specie['classification'] == 'sentient' or \
+                    specie['designation'] == 'sentient':
+                if specie['homeworld'] is not None:
+                    homeworld = requests.get(specie['homeworld'])
+                    planets.append(homeworld.json()['name'])
+
+        url = response.json()['next']
+    return planets
